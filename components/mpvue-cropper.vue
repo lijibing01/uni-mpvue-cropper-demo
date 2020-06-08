41388bf0e9dba98e426eb24df0dd4dad1fe1d51d
lijibing <635497669@qq.com>
2020-06-08 22:33:01 +0800

    初始化

---
+++ components/mpvue-cropper.vue
@@ -0,0 +1,109 @@
+<template>
+<div>
+  <canvas
+      v-if="_canvasId"
+      :canvasId="_canvasId"
+      @touchstart="touchstart"
+      @touchmove="touchmove"
+      @touchend="touchend"
+      disable-scroll
+      :style="{ width: _width + 'px', height: _height + 'px', background: 'rgba(0, 0, 0, .8)' }">
+  </canvas>
+  <canvas
+    v-if="_targetId"
+    :canvas-id="_targetId"
+    disable-scroll
+    :style="{
+      position: 'fixed',
+      top: -_width * _pixelRatio + 'px',
+      left: -_height * _pixelRatio + 'px',
+      width: _width * _pixelRatio + 'px',
+      height: _height * _pixelRatio + 'px'
+    }">
+  </canvas>
+</div>
+</template>
+
+<script>
+  import WeCropper from './we-cropper.js'
+
+  export default {
+    name: 'mpvue-cropper',
+    props: {
+      option: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        _wecropper: null
+      }
+    },
+    computed: {
+      _canvasId () {
+        return this.option.id
+      },
+      _targetId () {
+        return this.option.targetId
+      },
+      _width () {
+        return this.option.width
+      },
+      _height () {
+        return this.option.height
+      },
+      _pixelRatio () {
+        return this.option.pixelRatio
+      }
+    },
+    methods: {
+      touchstart ($event) {
+        this._wecropper.touchStart($event.mp)
+      },
+      touchmove ($event) {
+        this._wecropper.touchMove($event.mp)
+      },
+      touchend ($event) {
+        this._wecropper.touchEnd($event.mp)
+      },
+      pushOrigin (src) {
+        this._wecropper.pushOrign(src)
+      },
+      updateCanvas () {
+        this._wecropper.updateCanvas()
+      },
+      getCropperBase64 (fn) {
+        return this._wecropper.getCropperBase64(fn)
+      },
+      getCropperImage (opt, fn) {
+        return this._wecropper.getCropperImage(opt, fn)
+      },
+      init () {
+        this._wecropper = new WeCropper(Object.assign(this.option, {
+          id: this._canvasId,
+          targetId: this._targetId,
+          pixelRatio: this._pixelRatio,
+		  vueThis: this
+        }))
+        .on('ready', (...args) => {
+          this.$emit('ready', ...args)
+        })
+        .on('beforeImageLoad', (...args) => {
+          this.$emit('beforeImageLoad', ...args)
+        })
+        .on('imageLoad', (...args) => {
+          this.$emit('imageLoad', ...args)
+        })
+        .on('beforeDraw', (...args) => {
+          this.$emit('beforeDraw', ...args)
+        })
+      }
+    },
+    mounted () {
+      if (!this.option) {
+        return console.warn('[mpvue-cropper] 请传入option参数\n参数配置见文档：https://we-plugin.github.io/we-cropper/#/api')
+      }
+      this.init()
+    }
+  }
+</script>
---
+++ pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="content">
+		<cropper :option="cropperOption" ref="cropper"></cropper>
+	</view>
+</template>
+
+<script>
+	import cropper from '../../components/mpvue-cropper.vue'
+	export default {
+		components:{
+			cropper
+		},
+		data() {
+			return {
+				cropperOption:{
+					id: 'canvasId',
+					targetId: 'targetId',
+					pixelRatio: wx.getSystemInfoSync().pixelRatio
+				}
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		},
+		mounted:function(){
+			this.$refs.cropper.pushOrigin('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1591633864720&di=ebfa3b33add1640409e50b36c9fcc266&imgtype=0&src=http%3A%2F%2Fa2.att.hudong.com%2F36%2F48%2F19300001357258133412489354717.jpg')
+		}
+	}
+</script>
+
+<style>
+	.content {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
